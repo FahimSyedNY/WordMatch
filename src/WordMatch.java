9c3d688e259aec63a4a3e58cbda0576259f8f38a
FahimSyed <fahims13@nycstundents.net>
2025-10-22 11:48:01 -0400

    Initial

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class WordMatch {
+    private String secret;
+    //Input lowercase word
+    public WordMatch(String word) {
+        secret = word;
+    }
+    //Returns a guess
+    public int scoreGuess(String guess) {
+        int score = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++) {
+            if(secret.substring(i, i + guess.length()).equals(guess)) score++;
+        }
+        return score * guess.length() * guess.length();
+    }
+    // returns better of two guesses
+    public String findBetterGuess(String guess1, String guess2) {
+        return null;
+    }
 }
