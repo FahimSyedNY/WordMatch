7bf5be763dee4a6df02221586734dab78c04af88
FahimSyedNY <fahims13@nycstudents.net>
2025-10-23 05:15:44 -0400

    Added ABCTEST

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,20 @@ public class Main {
         System.out.println(w.scoreGuess("iss"));
         System.out.println(w.scoreGuess("issipp"));
         System.out.println(w.scoreGuess("mississippi"));
+        WordMatch game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+        WordMatch abctest = new WordMatch("");
+        System.out.println(abctest.findBetterGuess("z", "a"));
+        System.out.println(abctest.findBetterGuess("a", "z"));
+        System.out.println(abctest.findBetterGuess("bbba", "a"));
+        System.out.println(abctest.findBetterGuess("a", "bbba"));
+        System.out.println(abctest.findBetterGuess("abc", "cba"));
+        System.out.println(abctest.findBetterGuess("cba", "abc"));
+
     }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -8,12 +8,15 @@ public class WordMatch {
     public int scoreGuess(String guess) {
         int score = 0;
         for (int i = 0; i <= secret.length() - guess.length(); i++) {
-            if(secret.substring(i, i + guess.length()).equals(guess)) score++;
+            if(secret.startsWith(guess, i)) score++;
         }
         return score * guess.length() * guess.length();
     }
     // returns better of two guesses
     public String findBetterGuess(String guess1, String guess2) {
-        return null;
+        if (scoreGuess(guess1) > scoreGuess(guess2)) return guess1;
+        if (scoreGuess(guess2) > scoreGuess(guess1)) return guess2;
+        if (guess1.compareTo(guess2) > 0) return guess2;
+        return guess1;
     }
 }
