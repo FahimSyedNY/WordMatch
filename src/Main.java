551f57666b61a341fc45639cf8b0327d0f30b87a
FahimSyedNY <fahims13@nycstudents.net>
2025-11-03 20:47:41 -0500

    - Fixed alphabetical hierarchy - Added 1000 words total score calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         WordMatch w = new WordMatch("mississippi");
         System.out.println(w.scoreGuess("i"));
         System.out.println(w.scoreGuess("iss"));
@@ -19,6 +23,23 @@ public class Main {
         System.out.println(abctest.findBetterGuess("a", "bbba"));
         System.out.println(abctest.findBetterGuess("abc", "cba"));
         System.out.println(abctest.findBetterGuess("cba", "abc"));
-
+        System.out.print("------------------\n\n\n------------------\n");
+        System.out.println("Total Score: " + totalScore());
+    }
+    public static int totalScore() throws FileNotFoundException {
+        File f = new File("Guesses.txt");
+        Scanner s = new Scanner(f);
+        String[] words;
+        WordMatch solver;
+        int score = 0;
+        while (s.hasNextLine()) {
+            words = s.nextLine().split(" ");
+            solver = new WordMatch(words[0]);
+//            Test what is stored in the vars:
+//            System.out.println("{" + words[0] + ", " + words[1] + ", " + words[2] + "}");
+//            System.out.println(solver.findBetterGuess(words[1], words[2]));
+            score += solver.scoreGuess(solver.findBetterGuess(words[1], words[2]));
+        }
+        return score;
     }
 }
